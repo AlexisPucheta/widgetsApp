d5e8e5df806fa9d4c645ab1965fe9558a05acdfb
Alexis Pucheta <alexispucheta22@gmail.com>
2025-04-03 12:35:44 -0300

    Refactor routing to use named routes for Home, Buttons, and Cards screens; enhance ButtonsScreen with a back button and improved layout.

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,13 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
+  static const String name = 'buttons_screen';
+
   const ButtonsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Buttons')),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.pop();
+        },
+        child: const Icon(Icons.arrow_back_ios_outlined),
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated')),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.alarm_add_outlined),
+              label: const Text('ElevatedIcon'),
+            ),
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.pedal_bike_outlined),
+              label: const Text('FilledIcon'),
+            ),
+            OutlinedButton(onPressed: () {}, child: const Text('Outlined')),
+            OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.outdoor_grill_outlined),
+              label: const Text('OutlinedIcon'),
+            ),
+            TextButton(onPressed: () {}, child: const Text('Text')),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.umbrella_outlined),
+              label: const Text('TextIcon'),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.access_alarm_outlined),
+            ),
+            //TODO Custom Button
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.access_alarm_outlined),
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(
+                  colors.primaryContainer,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
