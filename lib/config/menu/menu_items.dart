b5a230c3735223e084e3481a18d89be706d83e65
Alexis Pucheta <alexispucheta22@gmail.com>
2025-04-03 08:56:43 -0300

    Add MenuItems class and sample menu items for navigation

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final String link;
+
+  const MenuItems({
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.link,
+  });
+}
+
+const appMenuItems = <MenuItems>[
+  MenuItems(
+    title: 'Botones',
+    subtitle: 'Varios botones en Flutter',
+    icon: Icons.smart_button_outlined,
+    link: '/buttons',
+  ),
+  MenuItems(
+    title: 'Tarjetas',
+    subtitle: 'Un contenedor estilizado',
+    icon: Icons.credit_card,
+    link: '/cards',
+  ),
+];
